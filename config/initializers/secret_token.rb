264d0e3eb89a0b8140931310b54037fab0866a7f
Moiz K <moiz@heuretix.com>
2012-10-19 20:51:22 -0400

    Adding all of the files
    Kool-aid man

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '5d800da4fee3c91c379d0528f8d096085e1f19458bd8124c2f455abf89a2fdde025de26615afda82393ff877d13dce635627fe20007004218031d3211230196a'
